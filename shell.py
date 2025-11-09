c26212a32e31b24521017455206a3d8898970dfa
LateForever <albertgrzegrzolka124@gmail.com>
2025-11-09 18:53:02 +0100

    Implemented python shell script with 3 supported commands: type, echo, exit

---
+++ shell.py
@@ -0,0 +1,95 @@
+import sys
+import os
+import subprocess
+
+class Shell:
+    def __init__(self):
+        self.commands = {
+            "exit": self.cmd_exit,
+            "echo": self.cmd_echo,
+            "type": self.cmd_type
+        }
+        self.PATH = os.environ["PATH"].split(":")
+
+    def run(self):
+        while True:
+            sys.stdout.write("$ ")
+            user_input = input().strip()
+
+            if not user_input:
+                continue
+
+            parts = user_input.split()
+            command, args = parts[0], parts[1:]
+
+            handler = self.commands.get(command, self.cmd_not_found)
+            should_exit = handler(command, args)
+
+            if should_exit:
+                break
+
+    # ----------------------
+    # Command Implementations
+    # ----------------------
+
+    def cmd_exit(self, *_):
+        """Exits the program"""
+        return True
+
+    def cmd_echo(self, _cmd, args):
+        """Echo back user arguments."""
+        print(" ".join(args))
+
+    def cmd_type(self, cmd, _args):
+        """Show if a command is builtin."""
+
+        if cmd in self.commands:
+            print(f"{cmd} is a shell builtin")
+        else:
+            executable_path = self.find_executable_in_path(cmd)
+
+            if executable_path is None:
+                self.cmd_not_found(cmd)
+
+            else:
+                print(f"{cmd} is {executable_path}")
+
+    def cmd_not_found(self, cmd, args=None):
+        """Handle unknown commands."""
+
+        executable_path = self.find_executable_in_path(cmd)
+
+        if executable_path is None:
+            print(f"{cmd}: not found")
+            return
+
+        subprocess.run([cmd, *args])
+        return
+
+    def find_executable_in_path(self, executable_file_name):
+        """
+        Search directories listed in the PATH environment variable for an executable file.
+
+        Args:
+            cmd_name (str): Name of the executable to locate.
+
+        Returns:
+            str | None: Full path to the executable if found and executable, otherwise None.
+        """
+        found_potencial_file = False
+        potential_path = None
+
+        for folder_path in self.PATH:
+            potential_path = os.path.join(folder_path, executable_file_name)
+
+            if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                found_potencial_file = True
+                break
+
+        if found_potencial_file:
+            return potential_path
+        
+        return None
+
+if __name__ == "__main__":
+    Shell().run()
